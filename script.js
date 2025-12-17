76ee404773abcbfe39e06c6d060435659db96b27
Sabila105 <zalfarizqisabila100@gmail.com>
2025-12-17 20:59:30 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("formPendaftaran").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("💖 Pendaftaran berhasil! Terima kasih sudah mendaftar 💖");
+});
